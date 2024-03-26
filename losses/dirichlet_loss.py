fdad8a035837043664d24b94dcb6e03496f76af3
Feng <52739675+zzifer@users.noreply.github.com>
2024-03-26 21:00:04 +0800

    Update dirichlet_loss.py

--- losses/dirichlet_loss.py
+++ losses/dirichlet_loss.py
@@ -5,8 +5,10 @@ import torch.nn.functional as F
 from utils.registry import LOSS_REGISTRY
 
 
+# 计算两个张量在指定维度上的点积
 def cdot(X, Y, dim):
     assert X.dim() == Y.dim()
+    # torch.mul(X, Y) 计算逐元素乘积，然后 torch.sum(..., dim=dim) 在指定维度上求和，最终得到点积结果
     return torch.sum(torch.mul(X, Y), dim=dim)
 
 
@@ -21,6 +23,7 @@ class DirichletLoss(nn.Module):
         assert feats.dim() == 3
 
         if self.normalize:
+            # 对特征进行归一化
             feats = F.normalize(feats, p=2, dim=-1)
 
         de = cdot(feats, torch.bmm(L, feats), dim=1)
